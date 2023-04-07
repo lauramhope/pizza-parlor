e9ab59e123eb1bfd3483b8fce93e761272e5ffcb
Laura Hope <ssteamlaura@gmail.com>
2023-04-07 10:11:58 -0700

    Add hidden div section for total, add event handlers for pizza-size button and pizza-submit button.

--- js/scripts.js
+++ js/scripts.js
@@ -22,3 +22,17 @@ Pizza.prototype.fullPizza = function () {
 
 let pizza = new Pizza();
 
+function handleSizeSubmission(event) {
+  event.preventDefault();
+
+}
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+
+}
+
+window.addEventListener("load", function() {
+  document.querySelector("#size-select").addEventListener("click",handleSizeSubmission);
+  document.querySelector("#pizza-submit").addEventListener("click",handleFormSubmission); 
+})
